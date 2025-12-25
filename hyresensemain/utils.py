12586c21cd3bc40e9ba4b3ce5556664e9845ff2e
Dev Goswami <goswami.dev2016@gmail.com>
2025-12-25 21:19:50 +0530

    final7

--- hyresensemain/utils.py
+++ hyresensemain/utils.py
@@ -1,27 +1,24 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import render_to_string
 
 def send_otp_email(user, otp_code):
-    subject = 'Your OTP Code'
-    message = f'Your OTP code is: {otp_code}. This code will expire in 10 minutes.'
-    
-    # Optional: Use HTML template
-    html_message = render_to_string('emails/otp_email.html', {
-        'user': user,
-        'otp_code': otp_code
+    subject = "HyreSense OTP Verification"
+    from_email = settings.DEFAULT_FROM_EMAIL
+    to_email = [user.email]
+
+    text_content = f"Your OTP is {otp_code}. It will expire in 10 minutes."
+
+    html_content = render_to_string("emails/otp_email.html", {
+        "user": user,
+        "otp_code": otp_code,
     })
-    
+
     try:
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-            html_message=html_message,
-            fail_silently=False,
-        )
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to_email)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
         return True
     except Exception as e:
-        print(f"Failed to send email: {e}")
+        print("GoDaddy SMTP Error:", e)
         return False
